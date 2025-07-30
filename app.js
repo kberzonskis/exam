97a6cbbf7e68fb91fd4399a8f20ad5926533135d
kberzonskis <k.berzonskis@gmail.com>
2025-07-30 14:17:39 +0200

    14.17

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import express from "express"; 
+import Joi from "joi";
+
+
 const app = express()
 app.use(express.json()); 
 
@@ -22,26 +25,33 @@ const books = [
 
 /*
 crud: 
-crate - app.post 
-Read - app.get 
-Update - app.put
+crate - app.post() 
+Read - app.get()
+Update - app.put()
 Delete - app.delete()
 */
 app.get('/', (req,res) => { 
 res.send('gyvenimas')
 
 } )
-app.listen(3000,() => console.log('lostening of port 3000...')); 
+
 
 app.get("/demo", (req,res) => { res.send (['a', 'b', 'c'])}) 
-app.get("/demo/:id", (req,res) => {const id = req.params.id; res.send (id)})
-app.get("/demo/:cat/:id", (req,res) => {const cat = req.params.cat; const id = req.params.id; res.send (id,cat)})    
+
+app.get("/demo/:id", (req,res) => {const id = req.params.id; res.send ([id,cat])})
+
+app.get("/demo/:cat/:id", (req,res) => {const cat = req.params.cat; const id = req.params.id; res.send ([id,cat])})  
+
+
 app.get("/query", (req,res) => { const q = req.query;  res.send (q)} ) 
 
 
 //////////////get/////////////////
 
 app.get("/api/books", (req,res)=> {res.send(books)} )
+
+
+
 app.get("/api/books/:id", (req,res)=> {
     const book = books.find((book) => book.id === parseInt( req.params.id)); 
     if (!book) return res.status(404).send("Tokios book nera")
@@ -50,12 +60,49 @@ app.get("/api/books/:id", (req,res)=> {
 
 
 //////////////POST/////////////////
-app.post('/api/books', (req,res) => { 
-    const naujaKnyga = 
-    { id: books.length+1, author: req.body.author, title: req.body.title}; books.push(naujaKnyga); res.send(books) } )
+app.post('/api/books', (req, res) => {
+    const schema = Joi.object({
+        author: Joi.string().min(5).required(),
+        title: Joi.string().min(1).required()
+    });
+
+    const validation = schema.validate(req.body);
+
+    if (validation.error) {
+        res.status(400).send(validation.error.details[0].message);
+        return;
+    }
+    const naujaKnyga = {
+        id: books.length + 1,
+        author: req.body.author,
+        title: req.body.title
+    }
+    books.push(naujaKnyga);
+    res.send(books);
+});
+
+// PUT / UPDATE
+app.post('/api/books/:id', (req, res) => {
+    const book = books.find(book => book.id === parseInt(req.params.id));
+    if (!book) return res.status(404).send("Book not found ☹️");
+
+    const schema = Joi.object({
+        author: Joi.string().min(5).required(),
+        title: Joi.string().min(1).required()
+    });
+
+    const validation = schema.validate(req.body);
+
+    if (validation.error) {
+        res.status(400).send(validation.error.details[0].message);
+        return;
+    }
+    book.author = req.body.author,
+    book.title = req.body.title
+    res.send(books);
+});
 
 
 
 
-
-app.listen(3000,() => console.log('lisstening of port 3000...')); 
+app.listen(3000,() => console.log('listening of port 3000...')); 
